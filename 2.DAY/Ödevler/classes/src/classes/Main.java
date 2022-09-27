f4e7179fcc62eb73928937ac0ca38ac925d92006
DOCTHORhez <105459557+DOCTHORhez@users.noreply.github.com>
2022-09-27 17:09:27 +0300

    classes

---
+++ 2.DAY/Ödevler/classes/src/classes/Main.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();  
+	}
+
+}
