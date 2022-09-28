6615345909d22c37955b85283a9fc91ac12046bf
DOCTHORhez <105459557+DOCTHORhez@users.noreply.github.com>
2022-09-28 16:12:57 +0300

    conditionals

---
+++ 1.DAY/Ödevler/conditionals/src/conditionals/Main.java
@@ -0,0 +1,23 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 19;
+		if(sayi<20) {
+			System.out.println("Sayı 20 den küçüktür.");
+		}
+		
+		else if(sayi==20) {
+			System.out.println("Sayı 20 ye eşittir.");
+		}
+		
+		else {
+			System.out.println("Sayı 20 den küçük değildir.");
+		}
+		
+		
+
+	}
+
+}
