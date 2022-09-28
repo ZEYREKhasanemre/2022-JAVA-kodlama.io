5c7cac575f6c913302e1b3bfa07cd53fbd048f7b
DOCTHORhez <105459557+DOCTHORhez@users.noreply.github.com>
2022-09-28 16:02:59 +0300

    variables

---
+++ 1.DAY/Ödevler/variables/src/variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj="Öğrenci sayısı :";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+		
+	}
+
+}
