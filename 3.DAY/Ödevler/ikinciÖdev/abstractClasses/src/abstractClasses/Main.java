4aa097b411f2db7ec740713d2736d18c390f2cf1
DOCTHORhez <105459557+DOCTHORhez@users.noreply.github.com>
2022-10-09 19:07:33 +0300

    abstract

---
+++ 3.DAY/Ödevler/ikinciÖdev/abstractClasses/src/abstractClasses/Main.java
@@ -0,0 +1,12 @@
+package abstractClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		GameCalculator gameCalculator = new OlderGameCalculator();
+		gameCalculator.hesapla();
+
+	}
+
+}
